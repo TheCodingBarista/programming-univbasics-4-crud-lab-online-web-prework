052f2dd2645329f5abbc49a5313e42c505cc989f
Melissa Holt <melissa.holt21@gmail.com>
2019-10-13 18:05:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ruby_vocab = ["arrays", "hashes",]
+  ruby_vocab = ["arrays", "hashes", "strings", ""]
 end
 
 def add_element_to_end_of_array(array, element)

48d7cfb327ea6bf764e986e2186abbd71cc0a06c
Melissa Holt <melissa.holt21@gmail.com>
2019-10-13 19:13:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ruby_array = ["arrays", "hashes", "strings", "methods"]
+  ruby_array = ["elements", "hashes", "strings", "methods"]
   ruby_element = "arrays!"
-  ruby_array << "elements"
+  ruby_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)

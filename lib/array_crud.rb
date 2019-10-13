e85b087fb6c00c02eb90e69fb2668b7f72b212bc
Melissa Holt <melissa.holt21@gmail.com>
2019-10-13 19:40:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  ruby_array = ["wow", "I", "am", "really", "learning", "arrays!"]
   ruby_element = "wow"
   ruby_element = ruby_array.shift
 end

03c4d2cae27ef219bb93e747b05fbdc9cc0ce8e2
Melissa Holt <melissa.holt21@gmail.com>
2019-10-13 18:55:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   ruby_vocab = ["arrays", "hashes", "strings", "methods"]
   ruby_vocab << "elements"
-  p 
+  p ruby_vocab
 end
 
 def add_element_to_start_of_array(array, element)
